d84d6dc66dd1fa7b3c292345daa8baf227551e4d
Eileen Flores <leenfr84@gmail.com>
2016-08-29 09:55:14 -0500

    Continuacion de la clase 29 Agosto 2016
    Cómo modificar matrices

---
+++ Tarea Markdown.R
@@ -0,0 +1,7 @@
+install.packages("rmarkdown")
+
+
+#html
+
+
+
--- hola.R
+++ hola.R
@@ -99,3 +99,62 @@ m
 dim(m)
 attributes(m)
 
+
+
+
+#MATRICES
+
+m <- matrix(nrow=2,ncol=3)
+m
+
+dim(m)
+attributes(m)
+
+#Cambio de dimensiones de 2x3 a 3x2
+dim(m) <- c(3,2)
+m
+
+
+#Crear una matriz con datos
+m <- matrix(1:6,3,2)
+m
+
+
+m <- matrix(1:6,3,2,T)
+m
+
+m <- matrix(1:6,3,3,T)
+m
+class(m)
+str(m)
+
+
+dim(m) <- c(2,5)
+
+#Agregar elementos a una matriz
+x <- c(1,2,3)
+y <- c("a","b","c")
+z <- c(x,y)
+z
+
+#Pega filas
+m1 <- rbind(m,x)
+m1
+
+#Pega Columnas
+m2 <- cbind(m,x)
+m2
+
+m1 <- rbind(m,y)
+m1
+
+m2 <- cbind(m,y)
+m2
+
+
+#Tambien se puede solamente poniendo cbind(...) o rbind(...) sin la asignacion (<-); 
+#la diferencia es que con asignacion se guarda en la memoria y sin la asignacion se tendria q volver
+#a llamar a la funcion
+
+
+#la funcion vector, todos sus elementos son de la misma clase
